01d66704409f3b74bcf13004c097d6cbf07bc005
Crusty1Bread <crisvargas3213@gmail.com>
2020-03-01 21:22:01 -0800

    Finished the HELP screen and allowed for reverse input ("explain 1" and "1 explain")

--- src/response.java
+++ src/response.java
@@ -31,10 +31,14 @@ public class response {
 
                 if (ans.equals("ok") || ans.equals("bye")) {
                     break;
-                } if (ans.equals("explain1")) {
+                } if (ans.equals("explain1") || ans.equals("1explain")) {
                     System.out.println("\nThis will give you information about what a certain class is or about.\n\nEXAMPLE:\n\">AP Computer Science A\"\n\"This is a course where you will learn about the writing of Java programs in preparation for the AP exam\"\n");
-                } if (ans.equals("explain2")){
+                } if (ans.equals("explain2") || ans.equals("2explain")){
                     System.out.println("\nThis will give you information about the school schedule and when certain events.\n\nEXAMPLE:\n\">Tuesday's Schedule\"\n\"On Tuesdays there is:\n\tZero Period: 6:30 - 7:32\n\tFirst Period: 7:40 - 9:41\n\tThird Period: 9:49 - 10:49\n\tLunch: ...\"\n");
+                } if (ans.equals("explain3") || ans.equals("3explain")) {
+                    System.out.println("\nThis will give you information on where certain facilities are located in the STEM building.\n\nEXAMPLE:\n\">Where can I get lunch\"\n\"You can get lunch near the farthest end of the STEM building inside the cafeteria area.\"");
+                } if (ans.equals("explain4") || ans.equals("4explain")) {
+                    System.out.println("\nThis will give you contact information in order to communicate with certain teachers.\n\nEXAMPLE:\n\">What is Mr.Smith's email?\"\n\"You can contact Mr.Smith at 123456@cnusd.k12.ca.us.\"");
                 }
             }
         } else {
