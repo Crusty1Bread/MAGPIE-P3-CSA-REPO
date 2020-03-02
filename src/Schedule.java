8840220c07ff504676ffa8b02a6a9db5819b6270
jacobtoyou <59934767+jacobtoyou@users.noreply.github.com>
2020-03-02 11:54:41 -0800

    Add files via upload
    Started to input the overall structure/basic responses, need help making the code work with the main/response classes

---
+++ src/Schedule.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Schedule extends response {
+    public Schedule() {
+
+        {
+            if (ans.equals("period")) {
+                if (ans.equals("end")) {
+                    if (ans.equals("1")) {
+                        System.out.println("What day is it today?");
+                        ans = scan.nextLine();
+
+                    }
+
+                }
+                if (ans.equals("start")) {
+
+
+                }
+            }
+        }
+    }
+}
